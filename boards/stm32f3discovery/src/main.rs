029d7f125252a115770aef1f14b04338ceb7c29a
Philip Levis <pal@cs.stanford.edu>
2020-09-18 23:01:16 -0700

    AdcVirtual -> AdcVirtualized and Adc -> AdcDedicated.
    Add explanatory comments to ADC syscall capsule module.

--- boards/stm32f3discovery/src/main.rs
+++ boards/stm32f3discovery/src/main.rs
@@ -61,7 +61,7 @@ struct STM32F3Discovery {
         'static,
         VirtualMuxAlarm<'static, stm32f303xc::tim2::Tim2<'static>>,
     >,
-    adc: &'static capsules::adc::AdcVirtual<'static>,
+    adc: &'static capsules::adc::AdcVirtualized<'static>,
 }
 
 /// Mapping of integer syscalls to objects that implement syscalls.
